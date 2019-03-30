a0f9af9e64fc1f894c0908c99afffb0108104009
gkpty <gkardonski@gmail.com>
2019-03-30 04:47:41 -0500

    added docs and changed layout of index. added ghost nav

---
+++ js/ghost.js
@@ -0,0 +1,18 @@
+// Activate scrollspy to add active class to navbar items on scroll
+$('body').scrollspy({
+  target: '#mainNav',
+  offset: 50
+});
+
+// Collapse Navbar
+var navbarCollapse = function() {
+  if ($("#mainNav").offset().top > 50) {
+    $("#mainNav").addClass("navbar-shrink");
+  } else {
+    $("#mainNav").removeClass("navbar-shrink");
+  }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
